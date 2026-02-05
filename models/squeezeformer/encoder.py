5fad49ec81b96529bf73c8ea2d85cb724119e278
dndeik <denis_in@mail.ru>
2026-02-05 13:16:34 +0300

    SqueezeFormer added

---
+++ models/squeezeformer/encoder.py
@@ -0,0 +1,192 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+from models.common_modules.regularization import DropPath
+from models.common_modules.transformer_modules import SwiGLUFFN, GQASelfAttentionRelPos
+from models.squeezeformer.conv_module import ConvModule, CausalConv1d
+from models.common_modules.mamba import Mamba2, Mamba2Config
+from utils.masking import get_mask_vector, cut_masks_with_len_vector, fix_full_masked_lines
+
+# from conv_module import ConvModule, CausalConv1d
+# from mamba import Mamba2, Mamba2Config
+
+GLOBAL_EPS = 1e-4
+
+
+class Scaler(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(1, 1, d_model))
+        self.bias = nn.Parameter(torch.zeros(1, 1, d_model))
+
+    def forward(self, x):
+        return self.weight * x + self.bias
+
+
+class BaseEncoderBlock(nn.Module):
+    def __init__(self, type: str, d_model, n_heads, n_groups, chunk_size, left_context, right_context=0, conv_kernel=9, dropout=0.):
+        super().__init__()
+        self.chunk_size = chunk_size
+        self.left_context = left_context
+        self.right_context = right_context
+        self.n_heads = n_heads
+        assert type in ['attn', 'mamba'], "Type must be 'attn' or 'mamba'"
+        self.type = type
+        max_rel_pos = math.ceil((self.chunk_size + self.left_context + self.right_context) / self.chunk_size) * self.chunk_size
+
+        # self.pre_attn_scaler = Scaler(d_model)
+        if self.type == 'attn':
+            self.block = GQASelfAttentionRelPos(d_model, num_heads=n_heads, num_groups=n_groups, max_position=max_rel_pos, bias=False, dropout=dropout,)
+        elif self.type == 'mamba':
+            self.block = Mamba2(Mamba2Config(d_model=d_model, chunk_size=chunk_size, expand=1))
+        self.attn_norm = nn.LayerNorm(d_model, eps=GLOBAL_EPS)
+
+        # self.pre_first_ffn_scaler = Scaler(d_model)
+        self.first_ffn = SwiGLUFFN(d_model, factor=2)
+        self.first_ffn_norm = nn.LayerNorm(d_model, eps=GLOBAL_EPS)
+
+        # self.pre_conv_block_scaler = Scaler(d_model)
+        self.conv_block = ConvModule(d_model, conv_kernel, dropout=dropout)
+        self.conv_norm = nn.LayerNorm(d_model, eps=GLOBAL_EPS)
+
+        # self.pre_sec_ffn_scaler = Scaler(d_model)
+        self.sec_ffn = SwiGLUFFN(d_model, factor=2)
+        self.sec_ffn_norm = nn.LayerNorm(d_model, eps=GLOBAL_EPS)
+
+        self.drop_block = DropPath(dropout)
+
+    def forward(self, x, attn_mask):
+        B, L, D = x.shape
+
+        residual = x
+        # x = self.pre_first_ffn_scaler(x)
+        x = self.first_ffn_norm(x)
+        x = residual + self.drop_block(self.first_ffn(x))
+        
+        residual = x
+        # x = self.pre_attn_scaler(x)
+        x = self.attn_norm(x)
+        if self.type == 'attn':
+            padded_x = torch.cat((torch.zeros((B, self.left_context, D), device=x.device, dtype=x.dtype), x), dim=1)
+            x = self.block(padded_x, attn_mask=attn_mask)[:, self.left_context:, :]
+        else:
+            pad_len = (x.shape[1] + self.chunk_size-1) // self.chunk_size * self.chunk_size - x.shape[1]
+            padded_x = torch.cat((x, torch.zeros((B, pad_len, D), device=x.device, dtype=x.dtype)), dim=1)
+            x = self.block(padded_x)[0][:, :L, :]
+        x = residual + self.drop_block(x)
+        
+        residual = x
+        x = self.conv_norm(x)
+        # x = self.pre_conv_block_scaler(x)
+        x = residual + self.drop_block(self.conv_block(x))
+
+        residual = x
+        x = self.sec_ffn_norm(x)
+        # x = self.pre_sec_ffn_scaler(x)
+        x = x + self.drop_block(self.sec_ffn(x))
+
+        return x
+
+
+class AudioEncoder(nn.Module):
+    def __init__(self, inter_d_model, chunk_size, left_context_chunk_number, right_context_chunk_number, n_heads, n_groups, conv_kernel=9, layer_num=1, mamba_every_n_block=3, dropout=0.1):
+        super().__init__()
+        self.n_heads = n_heads
+        self.dropout = dropout
+        self.chunk_size = chunk_size
+        self.left_context = int(left_context_chunk_number * chunk_size)
+        self.right_context = int(right_context_chunk_number * chunk_size)
+
+        assert mamba_every_n_block != 0, "'mamba_every_n_block' can't be zero"
+        if mamba_every_n_block < 0:
+            print("'mamba_every_n_block' is less then zero, only GQA block will be used")
+        use_mamba = mamba_every_n_block > 0  # if "mamba_every_n_block" < 0 we use only GQA
+
+        first_downscaled_block = layer_num // 2 - 1
+        last_downscaled_block = layer_num - 2
+        self.downsample_factor = 2
+
+        self.start_full_blocks = nn.ModuleList()
+        self.downsampled_blocks = nn.ModuleList()
+        self.end_full_blocks = nn.ModuleList()
+        for i in range(layer_num):
+            block_type = "mamba" if (i+1) % mamba_every_n_block == 0 and use_mamba else "attn"
+
+            if i >= first_downscaled_block and i <= last_downscaled_block:
+                self.downsampled_blocks.append(BaseEncoderBlock(block_type, inter_d_model, n_heads, n_groups, self.chunk_size // self.downsample_factor, self.left_context // self.downsample_factor, self.right_context // self.downsample_factor, conv_kernel // self.downsample_factor + conv_kernel % 2, dropout))
+            elif i < first_downscaled_block:
+                self.start_full_blocks.append(BaseEncoderBlock(block_type, inter_d_model, n_heads, n_groups, self.chunk_size, self.left_context, self.right_context, conv_kernel, dropout))
+            else:
+                self.end_full_blocks.append(BaseEncoderBlock(block_type, inter_d_model, n_heads, n_groups, self.chunk_size, self.left_context, self.right_context, conv_kernel, dropout))
+
+        self.downsample_conv = CausalConv1d(inter_d_model, inter_d_model, self.downsample_factor + 1, stride=self.downsample_factor)
+
+    def _get_finished_mask(self, x, audio_len=None, downsample_factor=1):
+        B, L, D = x.shape
+        mask = get_mask_vector(self.chunk_size // downsample_factor,
+                                (L + self.left_context) // downsample_factor,
+                                self.left_context // downsample_factor,
+                                self.right_context // downsample_factor,
+                                device=x.device)
+
+        if audio_len is not None:
+            audio_len = audio_len // downsample_factor
+            masks = cut_masks_with_len_vector(mask, audio_len, self.left_context)
+        else:
+            masks = mask.unsqueeze(0).expand(B, -1, -1)  # (B, L, L)
+        masks = fix_full_masked_lines(masks)
+        masks = torch.repeat_interleave(masks, self.n_heads, 0)
+        return masks
+
+    def forward(self, x, audio_len=None):
+        full_blocks_masks = self._get_finished_mask(x, audio_len, 1)
+        downsampled_blocks_masks = self._get_finished_mask(x, audio_len, self.downsample_factor)
+
+        for module in self.start_full_blocks:
+            x = module(x, full_blocks_masks)
+        
+        residual = x
+        src_len = x.shape[1]
+        x = x.transpose(1, 2)
+        x = self.downsample_conv(x)
+        x = x.transpose(1, 2)
+        for module in self.downsampled_blocks:
+            x = module(x, downsampled_blocks_masks)
+        
+        x = x.transpose(1, 2)
+        x = F.interpolate(x, size=src_len, mode="nearest")
+        x = x.transpose(1, 2)
+
+        x = residual + x
+
+        for module in self.end_full_blocks:
+            x = module(x, full_blocks_masks)
+
+        return x
+
+
+def count_parameters(model):
+    parametrs_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Number of parameters: {parametrs_num} ({parametrs_num / (10 ** 6):.1f}M)")
+
+
+if __name__ == "__main__":
+    inter_d_model = 512
+    chunk_size = 80
+    n_heads = 8
+    layer_num = 10
+    dropout = 0.1
+    model = AudioEncoder(inter_d_model, chunk_size, 3, 0.5, n_heads, n_heads//2, 9, layer_num, -1, dropout).eval()
+    count_parameters(model)
+
+    x = torch.randn(3, 400, inter_d_model)
+    audio_len = torch.tensor([36, 400, 253])
+    print(x.shape)
+    full_out = model(x, audio_len)
+    print(full_out.shape)
+
+    # mask = get_mask_cycle(80, 270, 120)
+    # vectorized_mask = get_mask_vector(80, 270, 120, "cpu")
+    # print("Masks are same: ", torch.allclose(mask, vectorized_mask, atol=1e-8))
---
+++ models/squeezeformer/model.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+
+from models.common_modules.regularization import SpecAugment
+from models.squeezeformer.encoder import AudioEncoder
+from models.squeezeformer.conv_module import CausalConv1d
+from models.common_modules.feature_extractor import FeaturesExractor
+from utils.decoding import ctc_greedy_decode_batch
+
+# from encoder import AudioEncoder
+
+GLOBAL_EPS = 1e-4
+
+
+class TransposedConv(nn.Module):
+    def __init__(self, in_channels, out_channels, k_size=3, stride=1):
+        super().__init__()
+
+        self.conv = CausalConv1d(in_channels, out_channels, k_size, stride=stride, conv_style="full")
+
+    def forward(self, x):
+        """[B, T, C]"""
+        x = x.transpose(1, 2)
+        x = self.conv(x)  # [B, C, T//stride]
+        x = x.transpose(1, 2)
+        return x
+    
+
+class Decoder(nn.Module):
+    def __init__(self, num_vocab, embed_dim, hidden_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(num_vocab, embed_dim)
+        self.predictor = nn.LSTM(embed_dim, hidden_dim, bidirectional=False, batch_first=True, num_layers=1)
+
+
+    def forward(self, y, y_lengths):
+        emb = self.embedding(y)
+        y_lengths = y_lengths.cpu()
+        packed_seq = nn.utils.rnn.pack_padded_sequence(emb, y_lengths, batch_first=True, enforce_sorted=False)
+        packed_seq, _ = self.predictor(packed_seq)
+        out, _ = nn.utils.rnn.pad_packed_sequence(packed_seq, batch_first=True)
+        return out
+    
+    def infer(self, y, hidden=None):
+        emb = self.embedding(y)
+        out, hidden = self.predictor(emb, hidden)
+        return out, hidden
+
+
+class SimpleJoiner(nn.Module):
+    def __init__(self, enc_dim, dec_dim, joint_dim, num_vocab, bias=False, dropout=0.1):
+        super().__init__()
+        self.fc_enc = nn.Linear(enc_dim, joint_dim, bias=bias)
+        self.fc_pred = nn.Linear(dec_dim, joint_dim, bias=bias)
+        self.fc_out = nn.Linear(joint_dim, num_vocab, bias=bias)
+        self.act  = nn.SiLU()
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, enc_out, pred_out):
+        enc_out = self.fc_enc(enc_out).unsqueeze(2)
+        pred_out = self.fc_pred(pred_out).unsqueeze(1)
+    
+        T = enc_out.size(1)
+        U = pred_out.size(2)
+
+        enc_exp = enc_out.expand(-1, T, U, -1)  # [B, T, U, joint_dim]
+        pred_exp = pred_out.expand(-1, T, U, -1)  # [B, T, U, joint_dim]
+
+        joint = enc_exp + pred_exp # [B, T, U, joint_dim]
+        joint = self.act(joint)
+        # joint = self.dropout(joint)
+
+        out = self.fc_out(joint)  # [B, T, U, num_vocab]
+        return out
+
+
+class ConformerHybrid(nn.Module):
+    def __init__(self, num_vocab, encoder_d_model,
+                 predictor_d_model, joiner_d_model, freq_dim, n_mel, time_factor, n_heads, n_groups, chunk_size,
+                 left_context_chunk_number=1, right_context_chunk_number=0,
+                 layer_num=8, mamba_every_n_block=3, dropout=0.1):
+        super().__init__()
+        # Encoder part
+        downsampled_chunk_size = chunk_size // time_factor
+        self.features_extractor = FeaturesExractor(freq_dim, n_mel, chunk_size=chunk_size, eps=GLOBAL_EPS)
+        self.spec_aug = SpecAugment(0.05, 0.05, 5, 3)
+        self.downsample_conv = TransposedConv(n_mel, encoder_d_model, k_size=time_factor * 2 + 1, stride=time_factor)
+        self.encoder = AudioEncoder(encoder_d_model, downsampled_chunk_size, left_context_chunk_number, right_context_chunk_number, n_heads, n_groups, 9, layer_num, mamba_every_n_block, dropout)
+
+        # CTC head
+        self.ctc_conv = TransposedConv(encoder_d_model, encoder_d_model, 7)
+        self.ctc_classifier = nn.Sequential(nn.Linear(encoder_d_model, 2 * encoder_d_model),
+                                            nn.SiLU(),
+                                            nn.Dropout(dropout),
+                                            nn.Linear(2 * encoder_d_model, num_vocab))
+
+        # RNNT part
+        self.rnnt_decoder = Decoder(num_vocab, predictor_d_model, predictor_d_model)
+        self.rnnt_classifier = SimpleJoiner(encoder_d_model, predictor_d_model, joiner_d_model, num_vocab, dropout=dropout)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, audio_len, targets, targets_len):
+        # [B, C, T, F]
+        x = self.features_extractor(x)
+        x = self.spec_aug(x, audio_len)
+        x = self.downsample_conv(x)
+        # x = self.dropout(x)
+        enc_out = self.encoder(x, audio_len)
+        # enc_out = self.dropout(enc_out)
+
+        # CTC head
+
+        ctc_logits = self.ctc_conv(enc_out)
+        ctc_logits = self.dropout(ctc_logits)
+        ctc_logits = self.ctc_classifier(ctc_logits)
+
+        # RNNT head
+        dec_out = self.rnnt_decoder(targets, targets_len)
+        # dec_out = self.dropout(dec_out)
+        rnnt_logits = self.rnnt_classifier(enc_out, dec_out)
+
+        return ctc_logits, rnnt_logits
+
+    def run_encoder(self, x, audio_len):
+        # [B, C, T, F]
+        x = self.features_extractor(x)
+        x = self.spec_aug(x, audio_len)
+        x = self.downsample_conv(x)
+        enc_out = self.encoder(x, audio_len)
+        return enc_out
+
+    def infer_ctc(self, x, audio_len=None, blank_token_id=1024):
+        # [B, C, T, F]
+        x = self.features_extractor(x)
+        x = self.downsample_conv(x)
+        x = self.encoder(x, audio_len)
+        enc_out = self.post_norm(x)
+
+        # CTC head
+        ctc_logits = self.ctc_decoder.infer(enc_out)
+        ctc_logits = self.post_ctc_dec_norm(ctc_logits)
+        ctc_logits = self.ctc_classifier(ctc_logits)
+
+        out = ctc_greedy_decode_batch(ctc_logits, blank_token_id)
+
+        return out
+
+    def infer_rnnt(self, x, audio_len=None, blank_token_id=1024):
+        # [B, C, T, F]
+        assert x.shape[0] == 1, "Now only single file supported"
+
+        x = self.features_extractor(x)
+        x = self.downsample_conv(x)
+        x = self.encoder(x, audio_len)
+        enc_out = self.post_norm(x)
+
+        # RNNT greedy decoding
+        dec_out, hidden = self._run_decoder(torch.tensor(blank_token_id))
+        ans = []
+        max_gen_tokens = 10 # Redundant in most cases
+        for t in range(enc_out.shape[1]):
+            enc_out_t = enc_out[:, t: t + 1, :]
+            
+            step = 0
+            while step < max_gen_tokens:
+                step += 1
+                token = self._run_joiner(enc_out_t, dec_out)
+
+                if token.item() != blank_token_id:
+                    ans.append(token.item())
+                    dec_out, hidden = self._run_decoder(token, hidden)
+                    
+                else:
+                    break
+
+        return ans
+    
+    def _run_decoder(self, token, hidden=None):
+        token = token.unsqueeze(0).unsqueeze(0)
+        dec_out, hidden = self.rnnt_decoder.infer(token, hidden)
+        dec_out = self.post_rnnt_dec_norm(dec_out)
+
+        return dec_out, hidden
+    
+    def _run_joiner(self, enc_out, dec_out):
+        out = self.rnnt_classifier(enc_out, dec_out)
+        out = torch.argmax(torch.squeeze(out))
+        return out
+
+
+def count_parameters(model):
+    parametrs_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Number of parameters: {parametrs_num} ({parametrs_num / (10 ** 6):.1f}M)")
+
+
+if __name__ == '__main__':
+    torch.manual_seed(21)
+
+    time_factor = 4
+    chunk_size = 100
+    freq_dim = 161
+    model = ConformerHybrid(num_vocab=1025,
+                            encoder_d_model=768,
+                            predictor_d_model=768,
+                            joiner_d_model=768,
+                            freq_dim=freq_dim,
+                            n_mel=60,
+                            time_factor=time_factor,
+                            chunk_size=chunk_size,
+                            n_heads=12,
+                            n_groups=6,
+                            layer_num=12,
+                            dropout=0.1)
+
+    count_parameters(model)
+
+    max_len = 1500
+    dummy_input = torch.randn(2, 2, max_len, freq_dim)
+    lens = torch.tensor([max_len, 1200])
+    res = model.run_encoder(dummy_input, lens // time_factor)
+    print(f"There are NANs: {torch.isnan(res).any()}")
+    print(f"There are INFs: {torch.isinf(res).any()}")
+    print(res.shape)
